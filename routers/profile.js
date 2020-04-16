259f500bf45c8eb9a6558120fde4ce14360917ed
WanderSmits <wandersmits@gmail.com>
2020-04-16 12:12:12 +0200

    post dateSubmit to db

--- routers/profile.js
+++ routers/profile.js
@@ -5,14 +5,23 @@ const auth = require("../auth/middleware");
 const router = new Router();
 
 router.post(`/`, auth, async (req, res) => {
-  const { interval, notification, totalTime, userId } = req.body;
+  const { interval, notification, totalTime, userId, dateSubmit } = req.body;
 
-  console.log("What are my values?", interval, notification, totalTime, userId);
+  console.log(
+    "What are my values?",
+    interval,
+    notification,
+    totalTime,
+    userId,
+    dateSubmit
+  );
 
+  //creates new Profile table with the values given from the form
   const postProfile = await Profile.create({
     interval: interval,
+    duration_exercise: totalTime,
+    start_time: dateSubmit,
     notification: notification,
-    totalTime: totalTime,
     userId: userId,
   });
 
