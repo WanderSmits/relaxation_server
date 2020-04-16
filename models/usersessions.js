0bba6f7e3eeb333b12193f5f3ac5c2f9afa81195
WanderSmits <wandersmits@gmail.com>
2020-04-16 13:21:47 +0200

    on submit seed the table with user input

--- models/usersessions.js
+++ models/usersessions.js
@@ -1,10 +1,14 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const userSessions = sequelize.define('userSessions', {
-    userId: DataTypes.INTEGER,
-    sessionId: DataTypes.INTEGER
-  }, {});
-  userSessions.associate = function(models) {
+  const userSessions = sequelize.define(
+    "userSessions",
+    {
+      userId: DataTypes.INTEGER,
+      sessionId: DataTypes.INTEGER,
+    },
+    {}
+  );
+  userSessions.associate = function (models) {
     // associations can be defined here
   };
   return userSessions;
