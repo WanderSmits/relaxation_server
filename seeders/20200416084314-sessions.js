b62249ad1c95d7b1d713cab6e51eadd2f04817bb
WanderSmits <wandersmits@gmail.com>
2020-04-22 15:22:44 +0200

    made extra seed files to fill sessions

--- seeders/20200416084314-sessions.js
+++ seeders/20200416084314-sessions.js
@@ -20,6 +20,101 @@ module.exports = {
           createdAt: new Date(),
           updatedAt: new Date(),
         },
+        {
+          date: "2020-03-21",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-03-11",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-02-12",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-02-21",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-03-05",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-01-12",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-01-26",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-01-25",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-02-22",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-03-11",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-01-23",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-03-02",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-04-14",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-04-23",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-04-22",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-04-24",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-04-16",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-04-12",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          date: "2020-04-19",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
       ],
       {}
     );
